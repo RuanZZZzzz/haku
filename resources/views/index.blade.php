4a5154b0dfb389f2991293b6ea3ef8740d230197
yourname <251922508@qq.com>
2020-10-10 02:16:29 +0000

    初始化

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="zh-CN">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/bootstrap">
+    <title>Document</title>
+</head>
+<body>
+    <x-tou-bu></x-tou-bu>
+    <h1>我是主页</h1>
+</body>
+</html>
